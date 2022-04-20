eaa47de87772e906c1bf1cc05cda82b024f5e437
Xihai Luo <xihai.luo01@gmail.com>
2022-04-19 23:02:15 -0400

    create basic css and html templates

--- app.py
+++ app.py
@@ -1,11 +1,10 @@
-from flask import Flask
-from sqlalchemy import true
+from flask import Flask, render_template
 
 app = Flask(__name__)
 
 @app.route("/")
 def index():
-  return "hello world!"
+  return render_template("index.html")
 
 if __name__ == "__main__":
-  app.run(debug=true)
+  app.run(debug=True)
